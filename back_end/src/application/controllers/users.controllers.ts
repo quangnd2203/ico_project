ef67c4af6fc770ed52c55950b5e145cc6238d3f4
quangnd <quangnd.nta@gmail.com>
2024-07-29 16:59:39 +0700

    no message

--- back_end/src/application/controllers/users.controllers.ts
+++ back_end/src/application/controllers/users.controllers.ts
@@ -1,4 +1,6 @@
 import CreateUserDto from "src/domain/dtos/create_user.dtos.js";
+import { NetworkResponse, STATUS_CODE } from "src/domain/entities/network_response.entities.js";
+import CreateUserMapper from "src/domain/mappers/create_user.mappers.js";
 import IUsersController from "src/interface/controllers/users.controllers.js";
 import ICreateUserUseCases from "src/interface/usercases/users/create_user.usecases.js";
 
@@ -9,7 +11,12 @@ export default class UsersController implements IUsersController {
         this.createUserUseCase = createUserUseCase;
     }
 
-    async create(request: CreateUserDto): Promise<void> {
-        await this.createUserUseCase.execute(request);
+    async create(request: CreateUserDto): Promise<NetworkResponse<CreateUserDto>> {
+        try{
+            const user = await this.createUserUseCase.execute(request);
+            return NetworkResponse.success<CreateUserDto>(new CreateUserMapper().toResponse(user));
+        } catch (e) {
+            return NetworkResponse.fromErrors(STATUS_CODE.bad_request, e.message || 'create_user_error');
+        }
     }
 }
--- back_end/src/application/routers/users.routers.ts
+++ back_end/src/application/routers/users.routers.ts
@@ -3,38 +3,17 @@ import IUsersController from 'src/interface/controllers/users.controllers.js';
 import UsersController from '../controllers/users.controllers.js';
 import CreateUserUseCase from 'src/infrastructure/usercases/create_user.usercase.js';
 import UsersRepository from 'src/infrastructure/repositories/users.repositories.js';
-// // import * as controller from '../controllers/vault.controller.js';
 
+const userRouters = express.Router();
 
-// const userRouters = express.Router();
+const controller: IUsersController = new UsersController(
+    new CreateUserUseCase(
+        new UsersRepository(),
+    ),
+);
 
-// userRouters.post('/', async (req, res) => {
-//     // controller.withdraw(req).then((value) => res.status(value.code).send(value));
-// });
+userRouters.post('/', async (req, res) => {
+    controller.create(req.body).then((value) => res.status(value.code).send(value));
+});
 
-// export default userRouters;
-
-class UsersRouters {
-    private router: express.Router;
-    private controller: IUsersController;
-
-    constructor() {
-        this.router = express.Router();
-        this.controller = new UsersController(
-            new CreateUserUseCase(
-                new UsersRepository(),
-            ),
-        );
-    }
-
-    public init(): void {
-        this.router.post('/', async (req, res) => {
-            await this.controller.create(req.body);
-            res.status(201).send();
-        });
-    }
-
-    public getRouter(): express.Router {
-        return this.router;
-    }
-}
+export default userRouters;
