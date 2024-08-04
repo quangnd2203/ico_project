b5b914b9588a10c70d88242c0313d154efda660c
quangnguyen <quangnd.nta@gmail.com>
2024-08-05 01:13:06 +0700

    no message

---
+++ cms/src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { Layout } from './Layout';
+import { dataProvider } from './dataProvider';
+import { authProvider } from './authProvider';
+
+
+export const App = () => (
+    <Admin
+        layout={Layout}
+        dataProvider={dataProvider}
+		authProvider={authProvider}
+	>
+        
+    </Admin>
+);
+
+    
---
+++ cms/src/dataProvider.ts
@@ -0,0 +1,5 @@
+import use from 'ra-data-json-server';
+
+export const dataProvider = jsonServerProvider(
+    import.meta.env.VITE_JSON_SERVER_URL
+);
