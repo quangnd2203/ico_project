3cd0c8a8843caa40110c26c3b5e9691618527c0d
quangnguyen <quangnd.nta@gmail.com>
2024-08-06 02:16:24 +0700

    no message

--- cms/src/dataProvider.ts
+++ cms/src/dataProvider.ts
@@ -1,25 +1,36 @@
-import { create } from 'domain';
 import simpleRestProvider from 'ra-data-simple-rest';
 import { DataProvider } from 'react-admin';
+import axios from 'axios';
 
-const baseDataProvider = simpleRestProvider('http://localhost:3000/api');
+
+const BASE_URL = 'http://localhost:3000/api';
+
+const apiClient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+const baseDataProvider = simpleRestProvider(BASE_URL);
 
 export const dataProvider: DataProvider = {
     ...baseDataProvider,
     getList: async (resource, params) => {
-        // // const response: any = await baseDataProvider.getList(resource, params);
-        // // console.log(params);
-        // // if (resource === 'users') {
-        // //     response.data = response.data.response;
-        // // }
-        // // return response;
-        // return fetch(`/api/users?${params}`)
-        //     .then(response => response.json());
-        const request = await fetch(`http://localhost:3000/api/aaa`, { method: 'GET' });
-        var response = await request.json();
+        const query = {
+            keyword: params.filter.q,
+            page: params.pagination?.page,
+            limit: params.pagination?.perPage,
+            sortField: params.sort?.field,
+            sortOrder: params.sort?.order 
+        };
+        const response = await apiClient.get(`/${resource}`, { params: query });
+        console.log(response);
+        // const response: any = await baseDataProvider.getList(resource, query);
+        // response.data = response.data.response;
         return {
-            data: response.response,
-            total: 123
+            data: response.data.response,
+            total: response.headers['content-range'] ? parseInt(response.headers['content-range'].split(' ')[1]) : 0,
         };
     },
     create: (userId: String) => {
